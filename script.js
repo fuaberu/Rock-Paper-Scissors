5662970e52a459150eecc7505a3a84fd2080a5ce
fuaberu <kevinfabel@gmail.com>
2021-07-03 23:32:33 -0300

    Eddited the scriptp.js to work 1 time

---
+++ script.js
@@ -0,0 +1,28 @@
+// determine the function computerPlay with an array (rock,paper,scissors)
+    const computerPlay = ['rock','paper','scissors'];
+// make the computer choose randomly betwen 0 and 2
+    let random = Math.floor(Math.random() * computerPlay.length);
+// use the selection to choose one of the array options being the variable computerSlection
+    let computerselection = computerPlay[random];
+// ask for the player input stored as playerSelection. (should be case-insensitive)
+    let playerInput = prompt('What do you choose? Rock, Paper or Scissors?','')
+    let playerSelection = playerInput.toLowerCase()
+
+// compare the answers of the computer and the player to dicide who  wins the round
+    if (playerSelection === computerselection) 
+  {
+    alert('its a tie'); 
+  }
+    else if (playerSelection == 'rock' & computerselection == 'paper' || 
+    playerSelection == 'scissors' & computerselection == 'rock' || 
+    playerSelection == 'paper' & computerselection == 'scissors')
+  { alert('you lost')
+  } 
+    else if (playerSelection == 'paper' & computerselection == 'rock' || 
+    playerSelection == 'rock' & computerselection == 'scissors' || 
+    playerSelection == 'scissors' & computerselection == 'paper')
+      { alert('you won')
+  }
+// return a string declaring the winner
+
+console.log()
